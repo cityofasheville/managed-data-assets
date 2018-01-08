1da80b25b9fe53cc317aee567a5c73e158423d6d
Eric Jackson <ejackson@ashevillenc.gov>
2018-01-08 12:59:21 -0500

    added shape to v_simplicity_addresses

--- simplicity/v_simplicity_addresses/v_simplicity_addresses.sql
+++ simplicity/v_simplicity_addresses/v_simplicity_addresses.sql
@@ -8,7 +8,8 @@ CREATE OR REPLACE VIEW amd.v_simplicity_addresses AS
         latitude_wgs, longitude_wgs,
         trash_pickup_day, recycling_pickup_district, recycling_pickup_day, 
         zoning, owner_name, owner_address, owner_cityname, owner_state, 
-        owner_zipcode, property_pin, property_pinext, centerline_id, jurisdiction_type 
+        owner_zipcode, property_pin, property_pinext, centerline_id, jurisdiction_type,
+        shape 
  FROM amd.coa_bc_address_master
  WHERE location_type = 1
  
