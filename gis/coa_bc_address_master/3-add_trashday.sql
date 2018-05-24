d9310ebdd30aac170c1e6c121c90081fabd7ec53
Eric Jackson <ejackson@ashevillenc.gov>
2018-05-24 12:51:30 -0400

    AVL-RN: Add brushweek calculation to coa_bc_address_master

--- gis/coa_bc_address_master/3-add_trashday.sql
+++ gis/coa_bc_address_master/3-add_trashday.sql
@@ -3,19 +3,26 @@ UPDATE amd.coa_bc_address_master
 SET 
  trash_pickup_day = public_works_districts.truckday,
  recycling_pickup_district = public_works_districts.recdistrict,
- recycling_pickup_day = public_works_districts.recday
+ recycling_pickup_day = public_works_districts.recday,
+ brushweek = public_works_districts.brushweek
  FROM (
     SELECT DISTINCT
       amd.bc_location.location_id, 
-      amd.coa_districts_public_works.truckday,
-      amd.coa_districts_public_works.recdistrict,
-      amd.coa_districts_public_works.recday
+      pw.truckday,
+      pw.recdistrict,
+      pw.recday,
+      CASE
+        WHEN pw.brushweek LIKE 'opposite%' AND pw.recdistrict = 'A' THEN 'B'
+        WHEN pw.brushweek LIKE 'opposite%' AND pw.recdistrict = 'B' THEN 'A'
+        WHEN pw.brushweek IS NULL THEN NULL
+        ELSE pw.recdistrict
+      END as brushweek
     FROM
       amd.bc_location
     LEFT JOIN 
-      amd.coa_districts_public_works
+      amd.coa_districts_public_works as pw
     ON
-      st_contains(amd.coa_districts_public_works.shape,amd.bc_location.shape)
+      st_contains(pw.shape,amd.bc_location.shape)
   ) as public_works_districts 
 WHERE amd.coa_bc_address_master.location_id = public_works_districts.location_id
 
