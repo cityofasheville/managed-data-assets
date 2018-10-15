06f60b84acdb9df8cb1dfbe6914e234029a1f70d
Eric Jackson <eric@deepweave.com>
2018-10-15 09:22:53 -0400

    got rid of hard-coded paths

--- everbridge/cust.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-# Load employees and water customers to Everbridge
-"C:\Program Files\PuTTY\psftp.exe" -i "C:\Users\mssqlgisadmin\.ssh\City+of+Asheville+-+Citizens.ppk" -b "C:\coa\managed-data-assets\everbridge\cust.scr"
--- everbridge/emp.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-# Load employees and water customers to Everbridge
-"C:\Program Files\PuTTY\psftp.exe" -i "C:\Users\mssqlgisadmin\.ssh\City+of+Asheville+-+Employee.ppk"  -b "C:\coa\managed-data-assets\everbridge\emp.scr"
-
-#version using pageant:
-#"C:\Program Files\PuTTY\pageant.exe" "C:\Users\mssqlgisadmin\.ssh\City+of+Asheville+-+Citizens.ppk" "C:\Users\mssqlgisadmin\.ssh\City+of+Asheville+-+Employee.ppk"
-#"C:\Program Files\PuTTY\psftp.exe" -b "C:\jon\managed-data-assets-jon\everbridge\ftp_cust.scr"
-#"C:\Program Files\PuTTY\psftp.exe" -b "C:\jon\managed-data-assets-jon\everbridge\ftp_emp.scr"
