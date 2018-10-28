4d5396d6a6d1e4470df11ccd472826fafe40c6a0
Eric Jackson <eric@deepweave.com>
2018-10-28 15:47:51 -0400

    Fixed bug in bpt_testusers sql

--- core/pr_employee_info/create_bpt_testusers.sql
+++ core/pr_employee_info/create_bpt_testusers.sql
@@ -1,7 +1,6 @@
-insert into amd.pr_employee_info (objectid, emp_id, active, employee, emp_email, position, dept_id, department, div_id, division, sup_id, supervisor, sup_email, hire_date)
+insert into amd.pr_employee_info (emp_id, active, employee, emp_email, ft_status, position, dept_id, department, div_id, division, sup_id, supervisor, sup_email, hire_date)
 values (
-	100000, 9991, 'A', 'BPT TESTUSER1', 'bpt_testuser1@ashevillenc.gov', 'SYSTEMS ANALYST 1', '05', 'Information Technology', '054', 'IT Gis Appl Services', 6645, 'FRANCES RUIZ', 'fruiz@ashevillenc.gov', '2016-08-15'
+	9991, 'A', 'BPT TESTUSER1', 'bpt_testuser1@ashevillenc.gov', 'FT', 'SYSTEMS ANALYST 1', '05', 'Information Technology', '054', 'IT Gis Appl Services', 6645, 'FRANCES RUIZ', 'fruiz@ashevillenc.gov', '2016-08-15'
 ), (
-	100001, 9992, 'A', 'BPT TESTUSER2', 'bpt_testuser2@ashevillenc.gov', 'SYSTEMS ANALYST 1', '05', 'Information Technology', '054', 'IT Gis Appl Services', 6507, 'ERIC JACKSON', 'ejackson@ashevillenc.gov', '2016-08-15'
+	9992, 'A', 'BPT TESTUSER2', 'bpt_testuser2@ashevillenc.gov', 'FT', 'SYSTEMS ANALYST 1', '05', 'Information Technology', '054', 'IT Gis Appl Services', 6507, 'ERIC JACKSON', 'ejackson@ashevillenc.gov', '2016-08-15'
 );
-
