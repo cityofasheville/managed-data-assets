face6cbf7dda58eac8e6f2dfa05a3c500879f774
Jon Wilson <jontwilson@gmail.com>
2020-08-14 12:14:31 -0400

    Update update_test.sql

--- assets/fake_weekly_asset/update_test.sql
+++ assets/fake_weekly_asset/update_test.sql
@@ -1,6 +1,6 @@
-nsert into xjon.testtable(a,b,c,d) values (
+insert into xjon.testtable(a,b,c,d) values (
 123.456,
 true,
-'2020-08-13T13:24:44.100Z',
-'2020-08-13'
+'2020-06-25T17:22:44.100Z',
+'2020-05-25'
 );
