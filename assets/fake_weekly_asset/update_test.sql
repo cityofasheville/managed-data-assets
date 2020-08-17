4481eec25bb4b1ef0311358592404a196fa34b44
Jon Wilson <jontwilson@gmail.com>
2020-08-17 09:39:40 -0400

    Update update_test.sql

--- assets/fake_weekly_asset/update_test.sql
+++ assets/fake_weekly_asset/update_test.sql
@@ -1,1 +1,1 @@
-insert into xjon.testtable(a,b,c,d) values (random()X,array_to_string(ARRAY (SELECT substring('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ' FROM (random() *36)::int FOR 1) FROM generate_series(1, 12) ), '' ) ,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)
+insert into xjon.testtable(a,b,c,d) values (random(),array_to_string(ARRAY (SELECT substring('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ' FROM (random() *36)::int FOR 1) FROM generate_series(1, 12) ), '' ) ,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)
