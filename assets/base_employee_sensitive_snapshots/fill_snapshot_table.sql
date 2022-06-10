8af83b1bbfaf9b7ff52e9729a19cad34b5b2201d
Jon <jtwilson@ashevillenc.gov>
2022-06-10 11:54:35 -0400

    add annual_base_pay to base_emp snapshot

--- assets/base_employee_sensitive_snapshots/fill_snapshot_table.sql
+++ assets/base_employee_sensitive_snapshots/fill_snapshot_table.sql
@@ -4,11 +4,11 @@ legal_middle_name, legal_last_name, legal_name_suffix, primary_email, alt_email,
 department_id, department, position_name, ft_status, hire_date_original, hire_date_last, termination_date_last, 
 supervisor_id, supervisor_name, supervisor_email, is_supervisor, supervisee_count, phone_desk, phone_cell, 
 ad_username, ad_memberships, race_asian, race_black, race_native, race_pacific, race_white, ethnicity_hispanic, 
-gender, age, age_at_hire, age_at_termination)
+gender, age, age_at_hire, age_at_termination, annual_base_pay)
 select current_date as date_entered, employee_id, active, "sensitive", first_name, last_name, 
 legal_first_name, legal_middle_name, legal_last_name, legal_name_suffix, primary_email, alt_email, 
 division_id, division, department_id, department, position_name, ft_status, hire_date_original, 
 hire_date_last, termination_date_last, supervisor_id, supervisor_name, supervisor_email, is_supervisor, 
 supervisee_count, phone_desk, phone_cell, ad_username, ad_memberships, race_asian, race_black, race_native, 
-race_pacific, race_white, ethnicity_hispanic, gender, age, age_at_hire, age_at_termination
+race_pacific, race_white, ethnicity_hispanic, gender, age, age_at_hire, age_at_termination, annual_base_pay
 FROM standard.employee_sensitive;
